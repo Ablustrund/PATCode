e4c0bc759a1d741ba72519273102b73b90d0b39e
EnhydraGod <ddfishbonee@gmail.com>
2020-01-21 12:55:27 +0800

    简洁一些，就在草纸上找到规律就行了

--- PAT_B/PAT_B1003.cpp
+++ PAT_B/PAT_B1003.cpp
@@ -1,57 +1,29 @@
-#include <stdio.h>
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
-
-bool judge(string str)
-{
-    string aStr = "";
-    string tempStr = "PAT";
-    int pNum = 0, tNum = 0, aNum = 0;
-    for (int i = 0; i < str.size(); i++)
-    {
-        if(str[i] != 'P' && str[i] != 'A' && str[i] != 'T') return false;
-        if(str[i] == 'P') pNum++;
-        if(str[i] == 'T') tNum++;
-        if(str[i] == 'A') aNum++;
-    }
-    if((pNum != 1 && tNum != 1) || aNum == 0) return false;
-    if(str[0] == 'P' && str[str.size() - 1] == 'T') return true;
-
-    int j = 1;
-    for (aStr = 'A'; aStr.size() <= str.size() / 2; aStr += "A", j++)
-    {
-        tempStr = aStr + "PAT" + aStr;
-        while(tempStr.size() < str.size())
-        {
-            if(tempStr.size() > str.size()) break;
-            //int pPos = tempStr.find('P');
-            int tPos = tempStr.find('T');
-            int numTc = tempStr.size() - tPos;
-            string temp1 = tempStr;
-            string Pb = temp1.erase(0, aStr.size());
-            //cout << Pb << endl;
-            Pb = Pb.erase(tPos - j, numTc);
-            //cout << Pb << endl;
-            string Tc = tempStr.erase(0, tempStr.size() - numTc);
-            //cout << Tc << endl;
-            tempStr = aStr + Pb + "A" + Tc + aStr;
-            //cout << tempStr << endl;
-        }
-        if(tempStr == str) return true;
-    }
-    return false;
-}
-
+int n, pNum, tNum, aNum, flags;
+string str;
 int main()
 {
-    int n;
-    string str;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    getchar();
+    while(n--)
     {
-        cin >> str;
-        if(judge(str)) printf("YES\n");
-        else printf("NO\n");
+        getline(cin, str);
+        pNum = tNum = aNum = flags = 0;
+        for (int i = 0; i < str.size(); i++)
+        {
+            if(str[i] != 'P' && str[i] != 'A' && str[i] != 'T') {printf("NO\n"); flags = 1; break;}
+            if(str[i] == 'P') pNum++;
+            if(str[i] == 'T') tNum++;
+            if(str[i] == 'A') aNum++;
+        }
+        if(flags) continue;
+        if((pNum != 1 && tNum != 1) || aNum == 0) { printf("NO\n"); continue; }
+        int posP = str.find('P'), posT = str.find('T');
+        if(posP > posT) {printf("NO\n"); continue; }
+        if((posP == 0 && posT != str.size()-1) || (posP != 0 && posT == str.size()-1)){printf("NO\n"); continue; }
+        if(posT - posP >= 2 && ((posP == 0 && posT == str.size()-1) || ((str.size()-posT-1) % ((posT-posP-1)*posP) == 0))) { printf("YES\n"); continue; }
+        printf("NO\n");
     }
+    return 0;
 }
