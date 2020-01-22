e91e951daeae744ceb8f7ccaece095e62359a623
EnhydraGod <ddfishbonee@gmail.com>
2020-01-22 13:38:36 +0800

    pat

--- PAT_B/PAT_B1050.cpp
+++ PAT_B/PAT_B1050.cpp
@@ -1,55 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-long long int N, temp2, m, n;
-bool flag = 0;
-vector<long long int> nums;
-bool cmp(long long int a, long long int b)
+int N, temp, m, n, graph[10010][10010];
+vector<int> vec;
+bool cmp(const int &a, const int &b) {return a > b;}
+void getmn(int N, int &m, int &n)
 {
-    return a > b;
+    for (int i = (int)sqrt(N); i <= N; i++)
+        if(N % i == 0)
+        {
+            m = max(i, N/i);
+            n = N / m;
+            return;
+        }
 }
 int main()
 {
-    scanf("%lld", &N);
-    for (long long int i = 0; i < N; i++)
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++)
     {
-        scanf("%lld", &temp2);
-        nums.push_back(temp2);
+        scanf("%d", &temp);
+        vec.push_back(temp);
     }
-    sort(nums.begin(), nums.end(), cmp);
-    long long int temp = (long long int)sqrt(N);
-    if(temp * temp == N)
+    sort(vec.begin(), vec.end(), cmp);
+    getmn(N, m, n);
+    int bias = 0;
+    int k = 0, j = 0, p = 0;
+    for (int i = 0; i < m/2+1; i++)
     {
-        flag = 1;
-        m = n = temp;
+        p = bias;
+        for (j = bias; j < n - bias; j++) graph[p][j] = vec[k++];
+        if(k == vec.size()) break;
+        p = n - bias - 1;
+        for (j = bias+1; j < m - bias; j++) graph[j][p] = vec[k++];
+        if(k == vec.size()) break;
+        p = m - bias - 1;
+        for(j = n-bias-2; j >= bias; j--) graph[p][j] = vec[k++];
+        if(k == vec.size()) break;
+        p = bias;
+        for (j = m - bias - 2; j >= bias + 1; j--) graph[j][p] = vec[k++];
+        if(k == vec.size()) break;
+        bias++;
     }
-    temp++;
-    for (long long int i = temp; i <= N; i++)
-    {
-        if(flag) break;
-        for (long long int j = temp; j >= 1; j--)
-        {
-            temp2 = j * i;
-            if(temp2 > N) continue;
-            else if(temp2 == N)
-            {
-                m = i;
-                n = j;
-                flag = 1;
-                break;
-            }
-            else break;
-        }
-    }
-    long long countss = 0;
-    for (long long int i = 0; i < m; i++)
-    {
-        for (long long int j = 0; j < n; j++)
-        {
-            if (j == 0) printf("%lld", nums[countss++]);
-            else printf(" %lld", nums[countss++]);
-        }
-        printf("\n");
-    }
-        return 0;
+    for (int i = 0; i < m; i++) for(int j = 0; j < n; j++) if(j == 0) printf("%d", graph[i][j]); else printf(" %d", graph[i][j]);
+    printf("\n");
+    return 0;
 }
